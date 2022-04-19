6d73e28927be14b41cfe6243af8acca76a88816c
Tássis Fernando <tassis.fernandes@dtidigital.com.br>
2022-04-19 09:39:30 -0300

    Alterando constantes para 'final'

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -2,9 +2,9 @@ package util;
 
 public class Constants {
 
-    public static Double ZERO = 0D;
-    public static Double PASSO = 0.1D;
-    public static Double MAX_VALUE_X = 1000D;
-    public static Double MAX_VALUE_Y = 1000D;
-    public static Integer MAX_RESTRICOES = 5;
+    public static final Double ZERO = 0D;
+    public static final Double PASSO = 0.1D;
+    public static final Double MAX_VALUE_X = 1000D;
+    public static final Double MAX_VALUE_Y = 1000D;
+    public static final Integer MAX_RESTRICOES = 5;
 }
